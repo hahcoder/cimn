4759e3dd253ca644619e9ed11311c58e0623bd34
Ha Hoang <hah@mindarc.com.au>
2017-11-14 08:51:06 +0700

    Add code

---
+++ application/views/home.php
@@ -0,0 +1,10 @@
+<?php 
+defined('BASEPATH') OR exit('No direct script access allowed');
+ ?>
+<body>
+	<div class="main">
+		<p>
+			test
+		</p>
+	</div>
+</body>
---
+++ application/views/template/head.php
@@ -0,0 +1,9 @@
+<?php 
+	$this->load->helper('url'); 
+?>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title><?php echo $title ?></title>
+	<link rel="stylesheet" href="<?php echo base_url(); ?>skin/css/styles.css">
+</head>
