3dc4e493d9dd5bbc5e2a5a6eb7553033f277ee23
Ha Hoang <hahoang.coder@gmail.com>
2017-12-11 16:48:23 +0700

    Add Font Awesome

--- application/views/templates/head.php
+++ application/views/templates/head.php
@@ -10,11 +10,12 @@
 	<link href="<?php echo base_url(); ?>skin/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 	<link href="<?php echo base_url(); ?>skin/bootstrap/css/bootstrap-grid.min.css" rel="stylesheet">
 	<link href="<?php echo base_url(); ?>skin/bootstrap/css/bootstrap-reboot.min.css" rel="stylesheet">
-	<link rel="stylesheet" href="<?php echo base_url(); ?>skin/css/styles.css">
+	<link href="<?php echo base_url(); ?>skin/css/styles.css" rel="stylesheet">
 	
 	<!-- Js -->
+	<script src="<?php echo base_url(); ?>skin/js/jquery-3.2.1.min.js"></script>
 	<script src="<?php echo base_url(); ?>skin/bootstrap/js/bootstrap.min.js"></script>
 	<script src="<?php echo base_url(); ?>skin/bootstrap/js/bootstrap.bundle.min.js"></script>
-	<script src="<?php echo base_url(); ?>skin/js/jquery-3.2.1.min.js"></script>
+	<script src="<?php echo base_url(); ?>skin/fontawesome/js/fontawesome-all.min.js"></script>
 	<script src="<?php echo base_url(); ?>skin/js/custom.js"></script>
 </head>
--- application/views/templates/header_admin.php
+++ application/views/templates/header_admin.php
@@ -1,4 +1,11 @@
-<div class="navbar navbar-dark bg-dark">
-	<i class="fa fa-user" aria-hidden="true"></i>
-	<a href="#" class="nav-item">Log out</a>
+<div class="container-fluid bg-dark header">
+	<div class="row">
+		<div class="col"></div>
+		<div class="col text-right">
+			<div class="user-link">
+				<a href="#" class="nav-item text-light"><i class="fa fa-user" aria-hidden="true"></i>Log out</a>
+			</div>
+		</div>
+	</div>
+	
 </div>
