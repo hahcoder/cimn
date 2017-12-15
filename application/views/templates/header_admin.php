1a92f4ccbf392574b9425f17b66e16f53f86b98f
Ha Hoang <hahoang.coder@gmail.com>
2017-12-15 17:00:02 +0700

    Update Admin header

--- application/views/templates/header_admin.php
+++ application/views/templates/header_admin.php
@@ -1,14 +1,14 @@
 <div class="container header">
-	<div class="col-12 bg-dark">
+	<div class="col-12">
 		<div class="col"></div>
 		<div class="col text-right">
-			<div class="user-link text-light">
+			<div class="user-link text-dark">
 				<i class="fa fa-user" aria-hidden="true"></i>
 				<?php if($this->session->userdata('logged')): ?>
 					<?php if($this->session->userdata('user')) : ?>
 						<a href="#"><?php echo $this->session->userdata('user') ?></a>
 					<?php endif; ?>
-					<a href="<?php echo base_url().'admin/login/logout'; ?>" class="nav-item text-light">Log out</a>
+					<a href="<?php echo base_url().'admin/login/logout'; ?>" class="nav-item">Log out</a>
 				<?php endif; ?>
 			</div>
 		</div>
